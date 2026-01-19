dfc97f138566da4844a72dba3764a09d5a5c22f5
Paulo Machado <paulo.machado@redt.com.br>
2026-01-19 19:11:25 -0300

    Tentativa de Fazer o Vercel funcionar com o Firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+console.log("BOOT", {
+  hasKey: !!import.meta.env.VITE_FIREBASE_API_KEY,
+  keyLen: import.meta.env.VITE_FIREBASE_API_KEY?.length,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+});
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
